b6fa31255c2fd47a6e695c95bc22a51e9dcb2ab0
Leia Borges Santos de Jesus <LBSJesus@rd.com.br>
2022-02-06 11:28:55 -0300

    Inicio

---
+++ src/main/java/br/com/rd/controller/CRUDController.java
@@ -0,0 +1,131 @@
+package br.com.rd.controller;
+
+import java.io.IOException;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.rd.dao.UserDAO;
+import br.com.rd.model.User;
+
+
+
+/**
+ * Servlet implementation class CRUDController
+ */
+@WebServlet("/CRUDController")
+public class CRUDController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private UserDAO cont;
+	//private Integer teste = 0;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CRUDController() {
+    	super();
+        // TODO Auto-generated constructor stub
+        this.cont = new UserDAO();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String option = request.getParameter("option");
+		if (option == null) {
+			option = "qualquer coisa";
+		}
+		switch(option) {
+			case ("insertForm"):
+				showInsertUser(request, response);
+			break;
+			case ("updateForm"):
+				showUpdateUser(request, response);
+			break;
+			case ("update"):
+				updateUser(request, response);
+			break;
+			case ("delete"):
+				deleteUser(request, response);
+			break;
+			case ("insert"):
+				insertUser(request, response);
+			break;
+			default:
+				selectAllUsers(request, response);
+		}
+	}
+	
+	private void showInsertUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		request.getRequestDispatcher("form.jsp").forward(request, response);
+	}
+	
+	private void showUpdateUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		Integer id = Integer.parseInt(request.getParameter("id"));
+		User u = this.cont.selectById(id);
+		request.setAttribute("user", u);
+		request.getRequestDispatcher("form.jsp").forward(request, response);
+	}
+	
+	private void insertUser(HttpServletRequest tomate, HttpServletResponse alface) throws ServletException, IOException{
+		//String nomeBack = tomate.getParameter("nome"); // ERRO
+		String nomeBack = tomate.getParameter("name");
+		String emailBack = tomate.getParameter("email");
+		String paisBack = tomate.getParameter("pais");
+		if ((paisBack != null) && (nomeBack != null) && (emailBack != null)) {
+			if (!nomeBack.equals("")){
+				User user1 = new User(nomeBack, paisBack, emailBack);
+				this.cont.insert(user1);
+			}
+		}
+		alface.sendRedirect("CRUDController");
+	}
+	
+	private void selectAllUsers(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		//System.out.println(this.cont.selectAll());
+		request.setAttribute("listUser", this.cont.selectAll());
+		request.getRequestDispatcher("listTable.jsp").forward(request, response);
+	}
+	
+	private void deleteUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String idBack = request.getParameter("id");
+		if (idBack != null) {
+			Integer id = Integer.parseInt(idBack);
+			this.cont.removeUser(id);;
+		}
+		response.sendRedirect("CRUDController");
+	}
+	
+	private void updateUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nomeBack = request.getParameter("name");
+		String emailBack = request.getParameter("email");
+		String paisBack = request.getParameter("pais");
+		String idBack = request.getParameter("id");
+		if ((paisBack != null) && (nomeBack != null) && (emailBack != null) && (idBack != null)) {
+			if (!nomeBack.equals("")){
+				Integer id = Integer.parseInt(idBack);
+				User user1 = new User(nomeBack, paisBack, emailBack);
+				user1.setId(id);
+				this.cont.updateUser(user1);
+			}
+		}
+		response.sendRedirect("CRUDController");
+	}
+	
+}
